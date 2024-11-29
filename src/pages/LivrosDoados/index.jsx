4f1386678c2dfce6f5518d2cfcf0b9b0ef71bd09
MonicaAlvesP <monica52782@gmail.com>
2024-11-29 15:14:27 -0300

    :fire: Adição de mais livros, e mudança nos estilos da pagina livros doados

--- src/pages/LivrosDoados/index.jsx
+++ src/pages/LivrosDoados/index.jsx
@@ -3,10 +3,53 @@ import { ListBook } from '../../components/ListBook';
 import { mainContent, list } from './livrosDoados.module.scss';
 
 export const LivrosDoados = () => {
+
   return (
     <main className={mainContent}>
       <h1>Livros Doados</h1>
       <section className={list}>
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
+        <ListBook
+          image={BookCover}
+          name="O protagonista"
+          autor="Susanne Andrade"
+          category="Ficção"
+        />
         <ListBook
           image={BookCover}
           name="O protagonista"
