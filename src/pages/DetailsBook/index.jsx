9e06e6857db5061d9951e5e9f9e2ef7cf2215d13
MonicaAlvesP <monica52782@gmail.com>
2025-04-02 19:49:35 -0300

    :zap: adicionado backbutton para voltar pra pagina anterior

--- src/pages/DetailsBook/index.jsx
+++ src/pages/DetailsBook/index.jsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { getBookById } from "@/services/api";
+import { useNavigate } from "react-router-dom";
+import { IoIosArrowBack } from "react-icons/io";
 import s from "./detailsBook.module.scss";
 
 export const DetailsBook = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const [book, setBook] = useState(null);
   const [loading, setLoading] = useState(true);
@@ -40,18 +43,29 @@ export const DetailsBook = () => {
     return <p>Livro não encontrado.</p>;
   }
 
+  const handleBackButtonClick = () => {
+    navigate(-1);
+  }
+
   return (
     <main className={s.mainContent}>
       <section className={s.bookDetails}>
-        <h1>{book.titulo}</h1>
         <img
           className={s.bookCover}
           src={book.image_url}
           alt={`Capa do livro ${book.titulo}`}
         />
+        <h1>{book.titulo}</h1>
       </section>
 
       <section className={s.bookInfo}>
+        <button
+          onClick={handleBackButtonClick}
+          className={s.backButton}
+        >
+          <IoIosArrowBack />
+          Voltar
+        </button>
         <h2>Detalhes do Livro</h2>
         <p><strong>Autor:</strong> {book.autor}</p>
         <p><strong>Ano de Lançamento:</strong> {book.ano_lancamento}</p>
